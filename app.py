4b86ca97b5ea711033e072f0ead5bb6d123fd674
Robert <robert.pastor0691@orange.fr>
2026-06-04 15:31:33 +0200

    initial tarifa page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__ , static_url_path='/static')
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
